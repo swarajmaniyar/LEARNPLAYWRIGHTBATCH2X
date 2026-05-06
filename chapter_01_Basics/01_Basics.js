0897fe15abdcbd597cb0277bbadace4f54ee7aa7
Swaraj Maniyar <swaraj.maniyar@gmail.com>
2026-05-06 16:42:57 +0530

    Initial commit: Add Playwright basics chapters

---
+++ chapter_01_Basics/01_Basics.js
@@ -0,0 +1,3 @@
+console.log("Hello The Testing Academy");
+
+let x = 10;
---
+++ chapter_01_Basics/03_JS_Commands.js
@@ -0,0 +1,8 @@
+console.log(process.platform);
+//win32
+
+console.log(process.arch);
+//x64
+
+console.log("Node Version: ",process.version);
+//Node Version:  v24.14.1
